18d4c5d7b1c00adb1196a4056c1a0017e98bab5b
Mooove TV <mooveplus@gmail.com>
2020-10-24 23:24:16 +0100

    finished some pipes and guards stuff

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get, Res, Req, Post, Param, HttpException, HttpStatus, UseFilters, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Res, Req, Post, Param, HttpException, HttpStatus, UseFilters, ParseIntPipe, UsePipes, Body, UseGuards } from '@nestjs/common';
 import { Response, Request } from 'express';
 import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from '../exceptions/http-exception.filter';
+import { JoiValidationPipe } from 'src/pipes/validation.pipe';
+import { CreateCatDto } from './dtos/create-cat.dto';
+import { ObjectSchema } from "@hapi/joi";
+import { AuthGuard } from 'src/guards/auth.guard';
 
+ 
 @Controller('cats')
+@UseGuards(AuthGuard)
 export class CatsController {
 
     constructor(private catService: CatsService) {}
@@ -15,14 +21,24 @@ export class CatsController {
         })
     }
 
-    @Post()
-    @UseFilters(new HttpExceptionFilter())
-    create(): string {
-        return "This is a string response"
-    }
+    // @Post()
+    // @UsePipes(new JoiValidationPipe(createCatSchema))
+    // create(@Body() createCatDto: CreateCatDto) {
+    //     this.catService.create(createCatDto);
+    // }
 
     @Get(':cat_id')
     retrieveSingleCat(@Param('cat_id', ParseIntPipe) cat_id: number): string { // Using a pipe to validate the params value coming in
+        console.log(HttpStatus)
+        throw new HttpException({
+            status: HttpStatus.INTERNAL_SERVER_ERROR,
+            error: 'This is a custom message',
+        }, HttpStatus.INTERNAL_SERVER_ERROR); 
+    }
+
+    // Passing an in-place instance is useful if we want to customize the built-in pipe's behavior by passing options
+    @Get(':cat_id')
+    findOne(@Param('cat_id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE})) cat_id: number): string { // Using a pipe to validate the params value coming in
         console.log(HttpStatus)
         throw new HttpException({
             status: HttpStatus.INTERNAL_SERVER_ERROR,
---
+++ src/cats/dtos/create-cat.dto.ts
@@ -0,0 +1,14 @@
+import { isString } from "util";
+import { IsString, IsInt } from 'class-validator';
+
+
+export class CreateCatDto {
+    @IsString()
+    name: string;
+
+    @IsInt()
+    age: number;
+
+    @IsString()
+    breed: string;
+  }
